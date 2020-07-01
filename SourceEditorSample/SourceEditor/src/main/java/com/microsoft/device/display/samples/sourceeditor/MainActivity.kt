2bd41edb1bc43f9c6908a3f69527c0d382b46502
parkerschroeder <ptschroeder@wisc.edu>
2020-07-01 16:21:12 -0500

    Added file saving and switching support
    User can save file changes and choose to edit files using the action toolbar

--- SourceEditorSample/SourceEditor/src/main/java/com/microsoft/device/display/samples/sourceeditor/MainActivity.kt
+++ SourceEditorSample/SourceEditor/src/main/java/com/microsoft/device/display/samples/sourceeditor/MainActivity.kt
@@ -6,12 +6,148 @@
 
 package com.microsoft.device.display.samples.sourceeditor
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.microsoft.device.display.samples.sourceeditor.viewmodel.WebViewModel
+import java.io.*
+import java.nio.charset.Charset
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    // Intent request codes
+    val CREATE_FILE = 1
+    val PICK_TXT_FILE = 2
+
+    private lateinit var fileBtn: ImageView
+    private lateinit var saveBtn: ImageView
+    private lateinit var webVM: WebViewModel
+
+    fun createFile(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE, "test")
+
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivityForResult(intent, CREATE_FILE)
+    }
+
+    fun openFile(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+
+            // Optionally, specify a URI for the file that should appear in the
+            // system file picker when it loads.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+
+        startActivityForResult(intent, PICK_TXT_FILE)
+    }
+
+    @Throws(IOException::class)
+    private fun readTextFromUri(uri: Uri): String {
+        val stringBuilder = StringBuilder()
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    stringBuilder.append(line)
+                    line = reader.readLine()
+                }
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    private fun alterDocument(uri: Uri) {
+        try {
+            contentResolver.openFileDescriptor(uri, "w")?.use {
+                FileOutputStream(it.fileDescriptor).use {
+                    val charset: Charset = Charsets.UTF_8
+                    it.write(
+                            webVM.getText().value.toString()
+                                    .toByteArray(charset)
+                    )
+                }
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.component_action_toolbar)
+
+        webVM = ViewModelProvider(this).get(WebViewModel::class.java)
+
+        fileBtn = findViewById(R.id.btn_file)
+        fileBtn.setOnClickListener {
+            openFile(Uri.EMPTY)
+        }
+
+        saveBtn = findViewById(R.id.btn_save)
+        saveBtn.setOnClickListener {
+            val uri: Uri = Uri.EMPTY
+            createFile(uri)
+        }
+    }
+
+    override fun onActivityResult(
+            requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+            if (requestCode == CREATE_FILE
+                && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            resultData?.data?.also { uri ->
+                // Perform operations on the document using its URI.
+                alterDocument(uri)
+            }
+        }
+        if (requestCode == PICK_TXT_FILE
+                && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            resultData?.data?.also { uri ->
+                // Perform operations on the document using its URI.
+                val str: String = readTextFromUri(uri)
+                Log.d("VMDCODE", readTextFromUri(uri))
+
+                val builder = StringBuilder()
+                var initHeader = true
+
+                val lines = str.split("<")
+                lines.forEach{
+                    if (initHeader) {
+                        builder.append(it)
+                        initHeader = false
+                    } else {
+                        builder.append("<" + it + System.getProperty("line.separator"))
+                    }
+                }
+                webVM.setText(builder.toString())
+            }
+        }
     }
 }
