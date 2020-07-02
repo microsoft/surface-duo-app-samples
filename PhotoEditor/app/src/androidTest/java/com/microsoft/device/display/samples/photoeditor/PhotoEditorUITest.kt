6fb48be440c741571c5ab5e01473cffbd7001317
Kristen Halper <t-krhalp@microsoft.com>
2020-07-02 14:09:10 -0400

    Add more helper functions for switching screens

--- PhotoEditor/app/src/androidTest/java/com/microsoft/device/display/samples/photoeditor/PhotoEditorUITest.kt
+++ PhotoEditor/app/src/androidTest/java/com/microsoft/device/display/samples/photoeditor/PhotoEditorUITest.kt
@@ -1,8 +1,5 @@
 package com.microsoft.device.display.samples.photoeditor
 
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -12,14 +9,11 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import com.microsoft.device.dualscreen.layout.ScreenHelper
-import com.microsoft.device.dualscreen.layout.SurfaceDuoLayout
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -44,9 +38,14 @@ class PhotoEditorUITest {
 
     /**
      * Tests visibility of controls when app spanned vs. unspanned
+     *
+     * @precondition no other applications are open
      */
     @Test
     fun testControlVisibility() {
+        // Lock orientation for entire test so spanning/unspanning work as expected
+        device.setOrientationNatural()
+
         // App opens in single-screen mode, so dropdown and saturation slider should be visible while brightness and warmth sliders are hidden
         onView(withId(R.id.controls)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
         onView(withId(R.id.saturation)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
@@ -54,7 +53,6 @@ class PhotoEditorUITest {
         onView(withId(R.id.warmth)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
 
         spanFromLeft()
-        assert(isSpanned())
 
         // Switched to dual-screen mode, so dropdown should not exist and all sliders should be visible
         onView(withId(R.id.controls)).check(doesNotExist())
@@ -63,7 +61,7 @@ class PhotoEditorUITest {
         onView(withId(R.id.warmth)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
         unspanToLeft()
-        assertFalse(isSpanned())
+        device.unfreezeRotation()
     }
 
 
@@ -80,91 +78,97 @@ class PhotoEditorUITest {
      * See the link below for Surface Duo pixel information:
      * https://devblogs.microsoft.com/surface-duo/resource-configuration-for-microsoft-surface-duo/
      */
+
 //    @Test
 //    fun configureSpanning() {
+//        resetToLeftScreen()
+//
 //        spanFromLeft()
-//        assert(isSpanned())
+//        require(isSpanned())
 //
 //        unspanToRight()
-//        assertFalse(isSpanned())
+//        require(!isSpanned())
 //
 //        spanFromRight()
-//        assert(isSpanned())
+//        require(isSpanned())
 //
 //        unspanToLeft()
-//        assertFalse(isSpanned())
+//        require(!isSpanned())
+//
+//        switchToRight()
+//        switchToLeft()
 //    }
 
     companion object {
-        // your testing device
+        // testing device
         val device: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
-        // x-coordinate for middle of left screen
+        /**
+         * X-COORDINATES (pixels)
+         */
+        // middle of left screen
         const val leftX: Int = 675
 
-        // x-coordinate for middle of right screen
-        const val rightX: Int = 2105
+        // middle of right screen
+        const val rightX: Int = 2109
 
-        // y-coordinate for bottom of screen
-        const val bottomY: Int = 1780
+        // hinge area
+        const val middleX: Int = 1350
 
-        // x-coordinate for left of hinge area
-        const val leftMiddleX: Int = 1300
+        // left of hinge area
+        const val leftMiddleX: Int = 1200
 
-        // x-coordinate for right of hinge area
+        // right of hinge area
         const val rightMiddleX: Int = 1500
 
-        // y-coordinate for middle of screen
+        /**
+         * Y-COORDINATES (pixels)
+         */
+        // bottom of screen
+        const val bottomY: Int = 1780
+
+        // middle of screen
         const val middleY: Int = 900
 
-        // number of steps to take to complete a spanning swipe
+        /**
+         * ANIMATION STEPS
+         */
+        // panning swipe
         const val spanSteps: Int = 400
 
-        // number of steps to take to complete an unspanning swipe
+        // unspanning swipe
         const val unspanSteps: Int = 140
+
+        // switch from one screen to the other
+        const val switchSteps: Int = 50
     }
 
     private fun spanFromLeft() {
-        device.swipe(leftX, bottomY, leftMiddleX, middleY, spanSteps)
+        device.swipe(leftX, bottomY, middleX, middleY, spanSteps)
+        device.wait(Until.gone(By.clazz("SeekBar")), 3000)
     }
 
     private fun unspanToLeft() {
-        device.swipe(leftMiddleX, bottomY, leftX, middleY, unspanSteps)
+        device.swipe(rightMiddleX, bottomY, leftX, middleY, unspanSteps)
     }
 
     private fun spanFromRight() {
-        device.swipe(rightX, bottomY, rightMiddleX, middleY, spanSteps)
+        device.swipe(rightX, bottomY, middleX, middleY, spanSteps)
     }
 
     private fun unspanToRight() {
-        device.swipe(rightMiddleX, bottomY, rightX, middleY, unspanSteps)
+        device.swipe(leftMiddleX, bottomY, rightX, middleY, unspanSteps)
     }
 
-//    private fun isSpanned(): Matcher<View?> {
-//        return ScreenSizeMatcher()
-//    }
+    private fun switchToLeft() {
+        device.swipe(rightX, bottomY, leftX, middleY, switchSteps)
+    }
+
+    private fun switchToRight() {
+        device.swipe(leftX, bottomY, rightX, middleY, switchSteps)
+    }
 
     private fun isSpanned(): Boolean {
         return ScreenHelper.isDualMode(activityRule.activity)
     }
 }
-
-//class ScreenSizeMatcher() :
-//    TypeSafeMatcher<View?>(View::class.java) {
-//
-//    private val middle = 1392
-//
-//    override fun matchesSafely(target: View?): Boolean {
-//        if (target !is FrameLayout) {
-//            return false
-//        }
-//        // Compare layout size to single screen size
-//
-//        // notes: target.parent = SurfaceDuoLayout, not sure which properties to go to from there
-//        return target.width > middle
-//    }
-//
-//    override fun describeTo(description: Description) {
-//        description.appendText("isSpanned")
-//    }
-//}
