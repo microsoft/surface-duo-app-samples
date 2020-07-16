e3b67ce0d93901eb8638a75fb48b4050b970da13
parkerschroeder <ptschroeder@wisc.edu>
2020-07-16 10:56:32 -0500

    Added auto saving and loading
    any drawing changes are saved and loaded on fragment lifetime changes

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/SerializedStroke.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/SerializedStroke.kt
@@ -2,9 +2,9 @@ package com.microsoft.device.display.samples.twonote
 
 import java.io.Serializable
 
-data class SerializedStroke (
-        val xList: MutableList<Float> = mutableListOf(),
-        val yList: MutableList<Float> = mutableListOf(),
-        val pressureList: MutableList<Float> = mutableListOf(),
+data class SerializedStroke(
+        val xList: List<MutableList<Float>> = listOf(),
+        val yList: List<MutableList<Float>> = listOf(),
+        val pressureList: List<MutableList<Float>> = listOf(),
         val paintColor: Int = 0
 ) : Serializable
