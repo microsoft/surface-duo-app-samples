5aa7d81fbeb85eb44df40aa98eeb7fd919d2f901
parkerschroeder <ptschroeder@wisc.edu>
2020-07-20 15:42:29 -0500

    Improved single tab note saving
    Added coordination between notes and directory entries by implementing inodes

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/model/DrawViewModel.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/model/DrawViewModel.kt
@@ -7,25 +7,11 @@
 
 package com.microsoft.device.display.samples.twonote.model
 
-import android.graphics.Bitmap
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.microsoft.device.display.samples.twonote.Stroke
 
 class DrawViewModel : ViewModel() {
-    private val imageLiveData = MutableLiveData<Bitmap?>() // to copy between two screens
     private var strokeList = listOf<Stroke>() // to retain the drawing when spanning/unspanning
-    private var penRadius: Int = 0 // to retain the pen value when spanning/unspanning
-    private var identifier: String = ""
-
-    fun getImageLiveData(): LiveData<Bitmap?> {
-        return imageLiveData
-    }
-
-    fun setImageLiveData(bitmap: Bitmap?) {
-        imageLiveData.value = bitmap
-    }
 
     fun getStrokeList(): List<Stroke> {
         return strokeList
@@ -33,28 +19,5 @@ class DrawViewModel : ViewModel() {
 
     fun setStrokeList(s: List<Stroke>) {
         strokeList = s
-    }
-
-    fun getPenRadius(): Int {
-        return penRadius
-    }
-
-    fun setPenRadius(radius: Int) {
-        penRadius = radius
-    }
-
-    fun setIdentifier(id: String?) {
-        id?.let {
-            if (identifier != id) {
-                resetViewModel()
-                identifier = id
-            }
-        }
-    }
-
-    fun resetViewModel() {
-        strokeList = listOf()
-        penRadius = 0
-        identifier = ""
     }
 }
