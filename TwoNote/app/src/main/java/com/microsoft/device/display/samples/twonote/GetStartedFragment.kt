44426a50a75fee359d6643ec5f88b82ea859d9dd
Kristen Halper <khalp@vt.edu>
2020-07-22 11:36:31 -0400

    Add getting started screen for first span

---
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/GetStartedFragment.kt
@@ -0,0 +1,18 @@
+package com.microsoft.device.display.samples.twonote
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GetStartedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_get_started, container, false)
+    }
+}
