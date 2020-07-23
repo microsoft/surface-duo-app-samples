5633751f8760c787d9c5bf4c46f96d283c9ba9a9
parkerschroeder <ptschroeder@wisc.edu>
2020-07-23 13:48:51 -0500

    started categories backend
    changing DataProvider structure to support categories

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/model/DataProvider.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/model/DataProvider.kt
@@ -6,16 +6,50 @@
 
 package com.microsoft.device.display.samples.twonote.model
 
+import kotlin.math.max
+
 object DataProvider {
-    val inodes: MutableList<INode> = mutableListOf()
+    private val inodes: MutableList<INode> = mutableListOf()
+    private val categories: MutableList<INode> = mutableListOf()
+    private var highestINodeId = 0
+    private var highestCategoryId = 0
 
     /**
      * Add a new inode to the inode list
      */
     fun addINode(inode: INode) {
-        inodes.add(inode)
+        inodes.add(0, inode)
+        highestINodeId = max(inode.id, highestINodeId)
     }
     fun removeINode(inode: INode) {
         inodes.remove(inode)
+    }
+    fun getINodes(): MutableList<INode> {
+        return inodes
+    }
+    fun moveINodeToTop(inode: INode) {
+        if (inodes.remove(inode))
+            inodes.add(0, inode)
+    }
+    fun getNextInodeId(): Int {
+        return highestINodeId + 1
+    }
+
+    fun addCategory(inode: INode) {
+        categories.add(0, inode)
+        highestCategoryId = max(inode.id, highestCategoryId)
+    }
+    fun removeCategory(inode: INode) {
+        categories.remove(inode)
+    }
+    fun getCategories(): MutableList<INode> {
+        return categories
+    }
+    fun moveCategoryToTop(inode: INode) {
+        if (categories.remove(inode))
+            categories.add(0, inode)
+    }
+    fun getNextCategoryId(): Int {
+        return highestCategoryId + 1
     }
 }
