8537a33f0bee38b9f0d17283233955301e476732
parkerschroeder <ptschroeder@wisc.edu>
2020-07-31 15:16:42 -0500

    fullscreen drag and drop
    txt file can be dragged on any part of the screen

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/includes/Defines.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/includes/Defines.kt
@@ -6,21 +6,8 @@
  */
 
 object Defines {
-    // Scrolling values //
-    const val DEFAULT_RANGE = 1
-    const val MIN_RANGE_THRESHOLD = 100
-    const val EMPTY_BUFFER_SIZE = 0
-    const val DEFAULT_BUFFER_SIZE = 2
-    const val CODE_KEY = "Code"
-    const val PREVIEW_KEY = "Preview"
-
-    // Asset references //
-    const val DEFAULT_SOURCE_PATH = "source.html"
-    const val HTML_TYPE = "text/html"
-    const val ENCODING = "UTF-8"
-
     // Handler references //
-    const val DEFAULT_FILE_NAME = "code"
+    const val DEFAULT_FILE_NAME = "drawing"
     const val TEXT_PREFIX = "text/"
     const val PLAIN_TEXT = "text/plain"
 }
