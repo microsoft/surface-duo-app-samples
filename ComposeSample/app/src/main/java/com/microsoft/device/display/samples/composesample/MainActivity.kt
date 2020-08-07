5064ee821dfd33c0fe3b053ba4030586cf742ebe
Joy Liu <joyliu1@microsoft.com>
2020-08-07 16:23:57 -0700

    Use LayoutChangeCallback instead of manual checking

--- ComposeSample/app/src/main/java/com/microsoft/device/display/samples/composesample/MainActivity.kt
+++ ComposeSample/app/src/main/java/com/microsoft/device/display/samples/composesample/MainActivity.kt
@@ -8,27 +8,32 @@
 package com.microsoft.device.display.samples.composesample
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.platform.setContent
+import androidx.core.util.Consumer
 import androidx.lifecycle.ViewModelProvider
+import androidx.window.WindowLayoutInfo
 import androidx.window.WindowManager
 import com.microsoft.device.display.samples.composesample.ui.DuoSDKSampleAppsTheme
 import com.microsoft.device.display.samples.composesample.viewModels.AppStateViewModel
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var windowManager: WindowManager
     private lateinit var appStateViewModel: AppStateViewModel
+
+    private val handler = Handler(Looper.getMainLooper())
+    val mainThreadExecutor = Executor { r: Runnable -> handler.post(r) }
+    private val layoutStateChangeCallback = LayoutStateChangeCallback()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         windowManager = WindowManager(this, null)
         appStateViewModel = ViewModelProvider(this).get(AppStateViewModel::class.java)
 
         super.onCreate(savedInstanceState)
-
-        val isAppSpanned = isAppSpanned()
-        appStateViewModel.setIsScreenSpannedLiveData(isAppSpanned)
-        Log.i("ComposeSample", "onCreate, isAppSpanned: $isAppSpanned")
 
         setContent {
             DuoSDKSampleAppsTheme {
@@ -39,21 +44,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        windowManager.registerLayoutChangeCallback(mainThreadExecutor, layoutStateChangeCallback)
+        Log.i("ComposeSample", "onAttachedToWindow")
 
-        val isAppSpanned = isAppSpanned()
-        Log.i("ComposeSample", "onAttachedToWindow, isAppSpanned: $isAppSpanned")
-        appStateViewModel.setIsScreenSpannedLiveData(isAppSpanned)
     }
 
-    private fun isAppSpanned(): Boolean {
-        try {
-            val windowLayoutInfo = windowManager.windowLayoutInfo // only working after attached to window
-            if (windowLayoutInfo.displayFeatures.size > 0) {
-                return true
-            }
-        } catch (ex: Exception) {
-            return false
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        windowManager.unregisterLayoutChangeCallback(layoutStateChangeCallback)
+    }
+
+    inner class LayoutStateChangeCallback : Consumer<WindowLayoutInfo> {
+        override fun accept(newLayoutInfo: WindowLayoutInfo) {
+            val isAppSpanned = newLayoutInfo.displayFeatures.size > 0
+            appStateViewModel.setIsScreenSpannedLiveData(isAppSpanned)
+            Log.i("ComposeSample", "LayoutStateChangeCallback isScreenSpanned: $isAppSpanned")
         }
-        return false
     }
 }
