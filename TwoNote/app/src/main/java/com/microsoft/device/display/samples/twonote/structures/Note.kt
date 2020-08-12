a786d8cf2a435156efbb50eafdc0519a77e2b6dc
parkerschroeder <ptschroeder@wisc.edu>
2020-08-12 16:16:37 -0500

    cleaned up general app code

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/structures/Note.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/structures/Note.kt
@@ -8,8 +8,8 @@ package com.microsoft.device.display.samples.twonote.structures
 
 import java.io.Serializable
 
-class Note(var id: Int) : Serializable {
-    var title: String = "Note $id"
+class Note(var id: Int, localizedTitle: String = "Note") : Serializable {
+    var title: String = "$localizedTitle $id"
     var text: String = ""
     var drawings: List<SerializedStroke> = listOf()
     var images: List<SerializedImage> = listOf()
