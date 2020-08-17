ac09dffaea31581457bafe5ba409bc5c5b859867
parkerschroeder <ptschroeder@wisc.edu>
2020-08-17 09:50:22 -0500

    Added comments for models

--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/models/SerializedImage.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/models/SerializedImage.kt
@@ -9,6 +9,16 @@ package com.microsoft.device.display.samples.twonote.models
 
 import java.io.Serializable
 
+/**
+ * Serializable format containing image data
+ * Data can be used to recreate (deserialize) image bitmaps saved in memory
+ *
+ * @param name: unique file name associated with an image
+ * @param image: compressed bitmap string defining an image
+ * @param coords: x and y coordinates associated with an image
+ * @param dimens: width and height associated with an image
+ * @param rotated: rotation flag for an image - true if the image is rotated, false otherwise
+ */
 data class SerializedImage(
     val name: String,
     val image: String,
--- TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/models/Stroke.kt
+++ TwoNote/app/src/main/java/com/microsoft/device/display/samples/twonote/models/Stroke.kt
@@ -11,8 +11,12 @@ import Defines.DEFAULT_THICKNESS
 import android.graphics.Path
 import android.graphics.RectF
 
+/**
+ * Class that encapsulates continuous paths with different associated pressures
+ * Used to create a variable thickness line in inking and drawing events
+ */
 class Stroke {
-    // Serialized fields
+    // Serializable fields
     private var xList: MutableList<MutableList<Float>> = mutableListOf()
     private var yList: MutableList<MutableList<Float>> = mutableListOf()
     private var pressureList: MutableList<MutableList<Float>> = mutableListOf()
@@ -28,11 +32,35 @@ class Stroke {
     private var yCoord: Float = 0f
     private var prevPressure: Float = 0f
 
+    /**
+     * Create a new stroke with specified settings
+     *
+     * @param x: starting x coordinate of stroke
+     * @param y: starting y coordinate of stroke
+     * @param pressure: starting pressure of stroke
+     * @param color: hex color code associated with stroke
+     * @param thickness: thickness multiplier used for width calculations
+     * @param rotation: rotation flag for stroke - true if rotated, false otherwise
+     * @param highlight: highlighter flag for stroke - true if transparent, false by default
+     */
     constructor(x: Float, y: Float, pressure: Float, color: Int, thickness: Int, rotation: Boolean, highlight: Boolean = false) {
         initStroke(x, y, pressure, color, thickness, rotation, highlight)
     }
 
-    // reconstruct serialized data
+    /**
+     * Reconstruct a stroke using serialized data
+     *
+     * For each list of lists - each main index is associated with a specific path in the stroke
+     *                          each sub index is associated with a coordinate for a given path
+     *
+     * @param x: collection of x coordinates
+     * @param y: collection of y coordinates
+     * @param pressure: collection of pressure values
+     * @param color: hex color code for the stroke
+     * @param thickness: thickness multiplier for width calculations
+     * @param rotation: rotation flag for stroke - true if rotated, false otherwise
+     * @param highlight: highlighter flag for stroke - true if transparent, false otherwise
+     */
     constructor(
         x: List<MutableList<Float>>,
         y: List<MutableList<Float>>,
@@ -52,14 +80,20 @@ class Stroke {
                         continueDrawing(x[paths][coords], y[paths][coords], pressure[paths][coords])
                 }
             }
-            finishStroke()
+            finishPath()
         }
     }
 
     /**
+     * Initialize variables used for stroke creation
      *
-     *
-     * @param
+     * @param x: starting x coordinate of stroke
+     * @param y: starting y coordinate of stroke
+     * @param pressure: starting pressure of stroke
+     * @param color: hex color code associated with stroke
+     * @param thickness: thickness multiplier used for width calculations
+     * @param rotation: rotation flag for stroke - true if rotated, false otherwise
+     * @param highlight: highlighter flag for stroke - true if transparent, false by default
      */
     private fun initStroke(x: Float, y: Float, pressure: Float, color: Int, thickness: Int, rotation: Boolean, highlight: Boolean = false) {
         xCoord = x
@@ -70,13 +104,11 @@ class Stroke {
         rotated = rotation
         highlightStroke = highlight
 
-        addJoint(x, y, pressure)
+        addPath(x, y, pressure)
     }
 
     /**
-     *
-     *
-     * @param
+     * Initialize variables needed for a new path in this Stroke
      */
     private fun initPath() {
         xList.add(mutableListOf())
@@ -85,12 +117,15 @@ class Stroke {
     }
 
     /**
+     * Create a new path with a new pressure at given coordinates
+     * Connect this path to the last path (if it exists) to ensure continuity
      *
-     *
-     * @param
+     * @param x: x coordinate of new path endpoint
+     * @param y: y coordinate of new path endpoint
+     * @param pressure: pressure setting used for thickness calculation
      */
-    private fun addJoint(x: Float, y: Float, pressure: Float) {
-        finishStroke()
+    private fun addPath(x: Float, y: Float, pressure: Float) {
+        finishPath()
         initPath()
 
         val path = Path()
@@ -103,11 +138,12 @@ class Stroke {
     }
 
     /**
+     * Extend the current path with a constant pressure to given coordinates
      *
-     *
-     * @param
+     * @param x: x coordinate of new path endpoint
+     * @param y: y coordinate of new path endpoint
      */
-    private fun continueJoint(x: Float, y: Float) {
+    private fun continuePath(x: Float, y: Float) {
         if (pathList.isNotEmpty()) {
             pathList[pathList.lastIndex].lineTo(x, y)
             updateValues(x, y, prevPressure)
@@ -115,24 +151,24 @@ class Stroke {
     }
 
     /**
-     * A new coordinate has been added to this stroke, decide whether or not to add a new joint
+     * A new coordinate has been added to this stroke, decide whether or not to add a new path
      *
-     * @param x: x coordinate of joint added
-     * @param y: y coordinate of joint added
+     * @param x: x coordinate of path added
+     * @param y: y coordinate of path added
      * @param pressure: pressure setting used for thickness calculation
      */
     fun continueDrawing(x: Float, y: Float, pressure: Float) {
         if (pressure == prevPressure && pathList.isNotEmpty())
-            continueJoint(x, y)
+            continuePath(x, y)
         else
-            addJoint(x, y, pressure)
+            addPath(x, y, pressure)
     }
 
     /**
-     * Record coordinates and other data associated with a newly added joint to the stroke
+     * Record coordinates and other data associated with a newly added path to the stroke
      *
-     * @param x: x coordinate of joint added
-     * @param y: y coordinate of joint added
+     * @param x: x coordinate of path added
+     * @param y: y coordinate of path added
      * @param pressure: pressure setting used for thickness calculation
      */
     private fun updateValues(x: Float, y: Float, pressure: Float) {
@@ -146,9 +182,9 @@ class Stroke {
     }
 
     /**
-     * Drawing of this stroke has ended, calculate bounds now that paths will no longer be added
+     * Drawing of this path has ended, calculate bounds now that coordinates will no longer be added
      */
-    fun finishStroke() {
+    fun finishPath() {
         if (pathList.isNotEmpty()) {
             val bounds = RectF()
             pathList[pathList.lastIndex].computeBounds(bounds, true)
